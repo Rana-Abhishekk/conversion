fc784ac444f449678b9a61d0287eb640f1760ee1
Abhishek Singh Rana <abhishek17082001@gmail.com>
2023-04-01 17:46:34 +0530

    pre final commit

--- src/main/java/com/conversion/Repositories/CharacteristicsRepo.java
+++ src/main/java/com/conversion/Repositories/CharacteristicsRepo.java
@@ -1,9 +1,10 @@
 package com.conversion.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.conversion.conversionDto.Characteristics;
-
+@Repository
 public interface CharacteristicsRepo extends JpaRepository<Characteristics, String>{
 
 }
